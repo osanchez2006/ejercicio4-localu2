0eac1c24ccf73e7bebaca4e70ee491488ec151dd
osanchez <oscartosfergmail.com>
2024-11-28 15:44:44 +0100

    añadir proyecto proyecto1 con paquete area y classe area.java

---
+++ proyecto1/src/area/area.java
@@ -0,0 +1,18 @@
+package area;
+
+import java.util.Scanner;
+
+public class area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el radio del círculo: ");
+        double radio = scanner.nextDouble();
+
+        double area = Math.PI * Math.pow(radio, 2);
+        System.out.println("El área del círculo es: " + area);
+
+        scanner.close();
+    }
+}
+
